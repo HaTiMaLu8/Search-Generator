73ed1ad3f0cbae4fc33babeed68a647947740c6b
HaTiMaLu8 <github@hati8.ml>
2022-11-17 21:55:56 +0900

    fix

---
+++ js/gen.js
@@ -0,0 +1,29 @@
+let word = document.getElementById("search")
+
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function youtube() {
+    window.location.href = "https://www.youtube.com/results?search_query="+ word.value
+  }
