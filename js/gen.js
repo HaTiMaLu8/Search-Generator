3ad3fe5b9accca654156b5702b698bb1375787a3
HaTiMaLu8 <github@hati8.ml>
2022-12-07 22:50:46 +0900

    beta 1.0

--- js/gen.js
+++ js/gen.js
@@ -3,27 +3,27 @@ let word = document.getElementById("search")
 function google() {
     window.location.href = "https://google.com/search?q="+ word.value
   }
-function google() {
-    window.location.href = "https://google.com/search?q="+ word.value
+function yahoo() {
+    window.location.href = "https://search.yahoo.co.jp/search?p="+ word.value
   }
-function google() {
-    window.location.href = "https://google.com/search?q="+ word.value
+function bing() {
+    window.location.href = "https://www.bing.com/search?q="+ word.value
   }
-function google() {
-    window.location.href = "https://google.com/search?q="+ word.value
+function duckduckgo() {
+    window.location.href = "https://duckduckgo.com/?q="+ word.value
   }
-function google() {
-    window.location.href = "https://google.com/search?q="+ word.value
+function baidu() {
+    window.location.href = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd="+ word.value
   }
-function google() {
-    window.location.href = "https://google.com/search?q="+ word.value
+function yandex() {
+    window.location.href = "https://yandex.com/search/?text="+ word.value
   }
-function google() {
-    window.location.href = "https://google.com/search?q="+ word.value
-  }
-function google() {
-    window.location.href = "https://google.com/search?q="+ word.value
-  }
-function youtube() {
+  function youtube() {
     window.location.href = "https://www.youtube.com/results?search_query="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
+  }
+function google() {
+    window.location.href = "https://google.com/search?q="+ word.value
   }
